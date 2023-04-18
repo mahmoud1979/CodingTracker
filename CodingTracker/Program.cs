27cb3950895fda891d6233598d58946c0c70949e
mahmoud1979 <80478247+mahmoud1979@users.noreply.github.com>
2023-04-18 21:58:43 +0100

    again many modifications in the same time

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, hello vb.net i'm coming");
+Console.WriteLine("Hello, hello vb.net i'm coming");
+Console.WriteLine("Hello, hello vb.net i'm coming");
+Console.WriteLine("Hello, hello vb.net i'm coming");
+Console.WriteLine("Hello, hello vb.net i'm coming");
+
