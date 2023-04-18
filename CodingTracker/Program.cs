9dfc9b85a9fe6d01f1653e96c1fa68589f8d6a65
mahmoud1979 <80478247+mahmoud1979@users.noreply.github.com>
2023-04-18 22:03:21 +0100

    one more exemple to show deleting

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,7 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, hello vb.net i'm coming");
-Console.WriteLine("Hello, hello vb.net i'm coming");
-Console.WriteLine("Hello, hello vb.net i'm coming");
-Console.WriteLine("Hello, hello vb.net i'm coming");
-Console.WriteLine("Hello, hello vb.net i'm coming");
+Console.WriteLine("Hello, hello one more exemple to show deleting");
+
 
